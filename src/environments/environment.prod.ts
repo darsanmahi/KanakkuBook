c288ed9615d47efe612a68758448783c8fbb6a60
Team2point0-tech <67360350+Team2point0-tech@users.noreply.github.com>
2020-08-16 12:25:45 +0530

    Deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
-};
+         production: true,
+         firebaseConfig: {
+           apiKey: 'AIzaSyCuJMf_KeBoyXjadj2ddTY0VC3AZQZUAhc',
+           authDomain: 'kanakkubook-ba48d.firebaseapp.com',
+           databaseURL: 'https://kanakkubook-ba48d.firebaseio.com',
+           projectId: 'kanakkubook-ba48d',
+           storageBucket: 'kanakkubook-ba48d.appspot.com',
+           messagingSenderId: '851964931890',
+           appId: '1:851964931890:web:595124ea739e0e8f34f12b',
+           measurementId: 'G-D1235SSCHW',
+         },
+       };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig : {
     apiKey: "AIzaSyCuJMf_KeBoyXjadj2ddTY0VC3AZQZUAhc",
     authDomain: "kanakkubook-ba48d.firebaseapp.com",
